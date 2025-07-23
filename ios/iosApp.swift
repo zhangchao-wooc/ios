8ebc5dcefb060da7206e2ee36feb80123a177ff5
wooc <wooc@cz-robots.com>
2025-07-23 10:30:45 +0800

    init: Init project.

---
+++ ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by wooc on 2025/7/23.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iosTests/iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  iosTests.swift
+//  iosTests
+//
+//  Created by wooc on 2025/7/23.
+//
+
+import Testing
+@testable import ios
+
+struct iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
